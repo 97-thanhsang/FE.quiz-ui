df9b16b8b619be7841f55789e9f05c0578d25a4d
DEVSANG\DELL <anhsang31797@gmail.com>
2025-02-14 00:12:23 +0700

    update js

--- main.js
+++ main.js
@@ -159,7 +159,7 @@ let questions = [
   const quizProgress = document.querySelector('#progress');
   const quizProgressText = document.querySelector('#progress_text');
   const quizSubmit = document.querySelector('#quiz_submit');
-  const quizPrew = document.querySelector('#quiz_prew');
+  const quizPrev = document.querySelector('#quiz_prev');
   const quizNext = document.querySelector('#quiz_next');
   const quizCount = document.querySelector('.quiz_question h5');
   const quizAnswers = document.querySelectorAll('.quiz_question ul li');
@@ -169,17 +169,58 @@ let questions = [
   const quizTitle = document.querySelector('#quiz_title');
   let currentIndex = null;
   let listSubmit = [];
+  let listResults = [];
+  let isSubmit = false;
   const quiz = {
 
+    //#region Function
+    randomArray : function (array) {
+      return (array = array.sort(() => Math.random() - Math.random()));
+    },
+    //#endregion
 
     //#region Event
+
+        //#region Random
+        randomQuestion : function () {
+          questions = this.randomArray(questions);
+          questions.forEach((question,index) => {
+            question.answers = this.randomArray(question.answers);
+          });
+        },
+        //#endregion
+        
         //#region Submit
         handleSubmit : function(){
             quizSubmit.addEventListener("click",()=>{
-                
+              const progressLen = listSubmit.filter(item => item >= 0);
+              let correct = 0;
+                if (progressLen.length === questions.length) {
+                  questions.forEach((question,index) => {
+                    const result = results.find((r) => r.quiz_id === question.quiz_id);
+
+                    // Lấy đáp án từng câu
+                    if (question.answers[listSubmit[index]] === result.answer) {
+                      listResults[index] = listSubmit[index];
+                      correct++;
+                    }
+                    else
+                    {
+                      quizQuestions[index].classList.add("incorrect");
+                      listResults[index] = question.answers.indexOf(result.answer);
+                    }
+                  });
+                  isSubmit = true;
+                  this.handleProgress(correct);
+                }
+                else
+                {
+                  alert("Chưa chọn hết !");
+                }
             });
         },
         //#endregion
+        
         //#region Question
         handleQuestionList: function(){
             quizQuestions.forEach((question,index) => {
@@ -199,40 +240,65 @@ let questions = [
                     const selected = listSubmit[currentIndex];
                     // Call event Answer click
                     selected >= 0 && quizAnswers[selected].click();
+
+                    if (isSubmit) {
+                      this.renderResults();
+                    }
+
                 });
             });
             quizQuestions[0].click();
         },
         //#endregion
+        
         //#region Answer
         // Event Answer click
         handleAnswer : function (){
             quizAnswers.forEach((answer,index) => {
                 //Set event click cho từng câu trả lời
             answer.addEventListener("click",() => {
-                // Default Style cho câu trả lời
-                quizAnswers.forEach((item) => item.classList.remove("active"));
-                // Set Style cho câu trả lời
-                answer.classList.add("active");
-                // Set Style cho câu hỏi của câu trả lời
-                quizQuestions[currentIndex].classList.add("selected");
-                // Set Value câu trả lời cho câu hỏi
-                listSubmit[currentIndex] = index;
-                // Set value Progress
-                this.handleProgress();
-            }); 
+                if (!isSubmit) {
+                  // Default Style cho câu trả lời
+                  quizAnswers.forEach((item) => item.classList.remove("active"));
+                  // Set Style cho câu trả lời
+                  answer.classList.add("active");
+                  // Set Style cho câu hỏi của câu trả lời
+                  quizQuestions[currentIndex].classList.add("selected");
+                  // Set Value câu trả lời cho câu hỏi
+                  listSubmit[currentIndex] = index;
+                  // Set value Progress
+                  this.handleProgress();
+                }
+                else
+                {
+                  return;
+                }
+            });
             });
         },
         //#endregion
+        
         //#region Progress
         // Event Progress
-        handleProgress : function(){
+        handleProgress : function(correct){
+          const r = quizProgress.getAttribute("r");
+          let styleProgress = "";
+          let textProgress = "";
+          if (!isSubmit) {
             const progressLen = listSubmit.filter(item => item >= 0);
-            const r = quizProgress.getAttribute("r");
-            quizProgress.style =`stroke-dasharray: ${(2 * Math.PI * r * progressLen.length) / questions.length} 9999;`;
-            quizProgressText.innerText = `${progressLen.length}/${questions.length}`;
+            styleProgress =`stroke-dasharray: ${(2 * Math.PI * r * progressLen.length) / questions.length} 9999;`;
+            textProgress = `${progressLen.length}/${questions.length}`;
+          }
+          else
+          {
+            styleProgress =`stroke-dasharray: ${(2 * Math.PI * r * correct) / questions.length} 9999;`;
+            textProgress = `${correct}/${questions.length}`;
+          }
+          quizProgress.style = styleProgress;
+          quizProgressText.innerText = textProgress;
         },
         //#endregion    
+        
         //#region Next
         handleNext : function(){
             quizNext.addEventListener("click",()=>{
@@ -243,8 +309,8 @@ let questions = [
                 quizQuestions[currentIndex].click();
             });
         },
-        handlePrew : function(){
-            quizPrew.addEventListener("click",()=>{
+        handlePrev : function(){
+          quizPrev.addEventListener("click",()=>{
                 --currentIndex;
                 if (currentIndex < 0) {
                     currentIndex = questions.length - 1;
@@ -253,8 +319,45 @@ let questions = [
             });
         },
         //#endregion
+    
+        //#region Key
+        handleKeyDown: function () {
+          document.addEventListener('keydown',(e)=>{
+            switch (e.key) {
+              case "ArrowRight":
+                return quizNext.click();
+              case "ArrowLeft":
+                 return quizPrev.click();
+              default:
+                return false;
+            }
+          });
+        },
+        handleKeyUp: function () {
+          
+        },
+        //#endregion
+
     //#endregion
+
     //#region Render
+        //#region Result
+        renderResults : function () {
+          if (listResults[currentIndex] === listSubmit[currentIndex]) {
+            quizAnswers.forEach((answer) => {
+              answer.classList.remove('incorrect');
+            });
+          }
+          else{
+            quizAnswers.forEach((answer) => {
+              answer.classList.remove('active');
+              answer.classList.remove('incorrect');
+            });
+            quizAnswers[listResults[currentIndex]].classList.add('active');
+            quizAnswers[listSubmit[currentIndex]].classList.add('incorrect');
+          }
+        },
+        //#endregion
         //#region Question
         // Render danh sách câu hỏi
         renderQuestionList : function (){
@@ -280,14 +383,65 @@ let questions = [
             quizProgressText.innerText = `0/${questions.length}`;
         },    
         //#endregion
+        //#region Timer
+        renderTimer: function () {
+          var timer = 15;
+          let _this = this;
+          // Lấy thẻ p có id là "timer"
+          var countdownElement = document.getElementById("timer");
+      
+          // Hàm cập nhật thời gian
+          function updateTimer() {
+            var minutes = Math.floor(timer / 60);
+            var seconds = timer % 60;
+      
+            // Định dạng thời gian thành chuỗi HH:MM:SS
+            var timerString =
+              (minutes < 10 ? "0" : "") +
+              minutes +
+              ":" +
+              (seconds < 10 ? "0" : "") +
+              seconds;
+      
+            // Gán thời gian đã định dạng vào thẻ p
+            countdownElement.innerHTML = timerString;
+      
+            // Giảm thời gian mỗi giây
+            timer--;
+            // Kiểm tra nếu hết thời gian
+            if (timer < 0) {
+              countdownElement.innerHTML = "Hết thời gian!";
+              _this.handleCheckResults();
+            }
+            if (isSubmit) {
+              clearInterval(intervalId);
+            }
+          }
+      
+          // Gọi hàm updateTimer mỗi giây
+          var intervalId = setInterval(updateTimer, 1000);
+        },
+        //#endregion
     //#endregion
+
+    render: function(){
+      this.renderQuestionList();
+      this.renderProgress();
+      this.renderTimer();
+    },
+    handle : function(){
+      this.handleQuestionList();
+      this.handleAnswer();
+      this.handleNext();
+      this.handlePrev();
+      this.handleSubmit();
+      this.handleKeyDown();
+      this.handleKeyUp();
+    },
     start: function (){
-        this.renderQuestionList();
-        this.renderProgress();
-        this.handleQuestionList();
-        this.handleAnswer();
-        this.handleNext();
-        this.handlePrew();
+      this.randomQuestion();
+      this.render();
+      this.handle();
     }
   };
   quiz.start();
